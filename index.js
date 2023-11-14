ab792b42da0a38780af6caa9e84378c539c197ed
Barshon <barshonweb@gmail.com>
2023-11-15 03:07:37 +0600

    server setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const cors=require('cors');
+const app=express();
+const port= process.env.PORT || 5000;
+
+//middlewares
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('motor fixer server is running');
+})
+
+app.listen(port,()=>{
+    console.log(`server listening at port ${port}`)
+})
