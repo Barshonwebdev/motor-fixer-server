82e877f13337665411531fabe14444bbf5a617df
Barshon <barshonweb@gmail.com>
2023-11-15 20:26:11 +0600

    data served dynamically in each service

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const cors=require('cors');
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app=express();
 const port= process.env.PORT || 5000;
 
@@ -34,6 +34,16 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    app.get("/services/:id", async(req,res)=>{
+      const id= req.params.id;
+      const query={_id:new ObjectId(id)};
+      const options={
+        projection: {_id:0,service_id:1,price:1, title:1, img:1,}
+      }
+      const result= await serviceCollection.findOne(query,options);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
